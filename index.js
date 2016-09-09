a3e2afad0efa13cb8e6314abe1131e438e72d607
michealjh <725694@qq.com>
2016-09-09 14:52:00 +0800

    wolai

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ function(id){
 	this.box = document.getElementById(id);
 	this.ddd = document.getElementsByTagName('input');
 }
-function(){}
+function(){
+	this.dddd = document.getElementById(id);
+}
